739c259e40d3223097999d4c192a5efa847bceef
tiiyuu <tiiyuu@users.noreply.github.com>
2021-06-25 07:59:35 +0000

    deploy: a8d06a016214994d4d369e7677c2a4262b79af22

--- admin/preview.js
+++ admin/preview.js
@@ -48,7 +48,7 @@ const BlogPreview = ({ entry, widgetFor }) => {
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="singleBlog__feature">
-                            <img src={ `/${data.postImage}` ? `/${data.postImage}` : 'images/cover.png' } alt="feature-image" />
+                            <img src={ `/${data.postImage}` || "images/cover.png" } />
                         </div>
                     </div>
                 </div>
