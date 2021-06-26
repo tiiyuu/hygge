409a04f09c8763f5d747fd9979e19d3d404ff455
tiiyuu <tiiyuu@users.noreply.github.com>
2021-06-26 11:33:53 +0000

    deploy: de4363d49ccdfde09c8b3d50f1fbb287d41bdd8b

--- js/resume_section_preview.js
+++ js/resume_section_preview.js
@@ -14,26 +14,26 @@ const ResumePostPreview = ({ entry, getAsset, widgetFor }) => {
                             </div>
 
                             <div class="btn-group nav mt-5">
-                                <a href="#${data.tab1Target}" class="btn btn-primary active" data-toggle="tab">{ data.tab1Name }</a>
-                                <a href="#${data.tab2Target}" class="btn btn-primary" data-toggle="tab">{ data.tab2Name }</a>
+                                <a href={`#${data.tab1Target}`} class="btn btn-primary active" data-toggle="tab">{ data.tab1Name }</a>
+                                <a href={`#${data.tab2Target}`} class="btn btn-primary" data-toggle="tab">{ data.tab2Name }</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="tab-content ">
-                            <div class="resume__education tab-pane active" id="${data.tab1Target}">
+                            <div class="resume__education tab-pane active" id={`${data.tab1Target}`}>
                                 { data.education.map(eda => (
                                     <div class="resume__education_item">
-                                        <span class="pre-line text-primary"> { eda.time } </span>
+                                        <span class="pre-line text-primary"> { eda.eda_time } </span>
                                         { widgetFor('eda_content') }
                                     </div>
                                 )) }
                             </div>
 
-                            <div class="resume__education tab-pane" id="${data.tab2Target}}">
+                            <div class="resume__education tab-pane" id={`${data.tab2Target}`}>
                                 { data.experience.map(exp => (
                                     <div class="resume__education_item">
-                                        <span class="pre-line text-primary"> { exp.time } </span>
+                                        <span class="pre-line text-primary"> { exp.exp_time } </span>
                                         { widgetFor('exp_content') }
                                     </div>
                                 )) }
