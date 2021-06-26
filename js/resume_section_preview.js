8e18ada7cb66a0fae417524279a9d96b51f19dfd
tiiyuu <tiiyuu@users.noreply.github.com>
2021-06-26 12:12:48 +0000

    deploy: 9c83f0ad06fbb5fe1b1e86b45e87c070d14536d1

--- js/resume_section_preview.js
+++ js/resume_section_preview.js
@@ -25,7 +25,7 @@ const ResumeSectionPreview = ({ entry, getAsset, widgetFor }) => {
                                 { data.education.map((eda) => (
                                     <div class="resume__education_item">
                                         <span class="pre-line text-primary"> { eda.time } </span>
-                                        { this.widgetFor('eda.content') }
+                                        { widgetFor('eda.content') }
                                     </div>
                                 )) }
                             </div>
