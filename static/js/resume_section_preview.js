92452b58f8b2021f17ccfceafdc694bfa206ed3e
tiiyuu <gces5816@gmail.com>
2021-06-26 20:51:20 +0800

    add { widgetFor('title') } in map

--- static/js/resume_section_preview.js
+++ static/js/resume_section_preview.js
@@ -1,13 +1,4 @@
-// const getEducation = (education, widgetFor) => {
-//     return widgetFor(education.content);
-// }
-
 const ResumeSectionPreview = ({ entry, getAsset, widgetFor }) => {
-    const self = this;
-    const getEducation = content => {
-        return widgetFor(content);
-    };
-
     const data = entry.get('data').toJS();
 
     return (
@@ -35,7 +26,7 @@ const ResumeSectionPreview = ({ entry, getAsset, widgetFor }) => {
                                 { data.education.map((eda) => (
                                     <div class="resume__education_item">
                                         <span class="pre-line text-primary"> { eda.time } </span>
-                                        { getEducation(eda.content) }
+                                        { widgetFor('title') }
                                     </div>
                                 )) }
                             </div>
