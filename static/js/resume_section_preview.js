ed4a583c23a0a19e16375f7175df3fbc8efd12db
tiiyuu <gces5816@gmail.com>
2021-06-26 19:17:38 +0800

    add resume preview

---
+++ static/js/resume_section_preview.js
@@ -0,0 +1,51 @@
+const ResumePostPreview = ({ entry, getAsset, widgetFor }) => {
+    const data = entry.get('data').toJS()
+    return (
+        <>
+        <section class="section resume" id="resume">
+            <div class="resume__background"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="resume__heading">
+                            <div class="text-light">
+                                <span class="top-title pre-line">{ data.topTitle }</span>
+                                { widgetFor('title') }
+                            </div>
+
+                            <div class="btn-group nav mt-5">
+                                <a href="#${data.tab1Target}" class="btn btn-primary active" data-toggle="tab">{ data.tab1Name }</a>
+                                <a href="#${data.tab2Target}" class="btn btn-primary" data-toggle="tab">{ data.tab2Name }</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="tab-content ">
+                            <div class="resume__education tab-pane active" id="${data.tab1Target}">
+                                { data.education.map(e => (
+                                    <div class="resume__education_item">
+                                        <span class="pre-line text-primary"> { e.time } </span>
+                                        { widgetFor('e.content') }
+                                    </div>
+                                )) }
+                            </div>
+
+                            <div class="resume__education tab-pane" id="${data.tab2Target}}">
+                                { data.experience.map(e => (
+                                    <div class="resume__education_item">
+                                        <span class="pre-line text-primary"> { e.time } </span>
+                                        { widgetFor('e.content') }
+                                    </div>
+                                )) }
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+
+CMS.registerPreviewTemplate('resume', ResumePostPreview)
