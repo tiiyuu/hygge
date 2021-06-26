28f773097e41cb1f171b4e703cde439edff5aebd
tiiyuu <gces5816@gmail.com>
2021-06-26 19:48:22 +0800

    map with widgetFor

--- static/js/resume_section_preview.js
+++ static/js/resume_section_preview.js
@@ -1,14 +1,3 @@
-// const getEducation = stars => {
-//     let content = [];
-//     for (let i = 0; i < stars; i++) {
-//         content.push(<li key={i}><i class="fa fa-star"></i></li>);
-//     }
-//     for (let i = stars; i < 5; i++) {
-//         content.push(<li key={i}><i class="fa fa-star inactive"></i></li>);
-//     }
-//     return content;
-// }
-
 const ResumeSectionPreview = ({ entry, getAsset, widgetFor }) => {
     const data = entry.get('data').toJS()
     return (
@@ -33,10 +22,10 @@ const ResumeSectionPreview = ({ entry, getAsset, widgetFor }) => {
                     <div class="col-lg-6">
                         <div class="tab-content ">
                             <div class="resume__education tab-pane active" id={`${data.tab1Target}`}>
-                                { data.education.map(eda => (
+                                { data.education.map((eda, widgetFor) => (
                                     <div class="resume__education_item">
                                         <span class="pre-line text-primary"> { eda.eda_time } </span>
-                                        
+                                        { widgetFor('eda.eda_content') }
                                     </div>
                                 )) }
                             </div>
@@ -45,7 +34,7 @@ const ResumeSectionPreview = ({ entry, getAsset, widgetFor }) => {
                                 { data.experience.map(exp => (
                                     <div class="resume__education_item">
                                         <span class="pre-line text-primary"> { exp.exp_time } </span>
-                                        
+
                                     </div>
                                 )) }
                             </div>
