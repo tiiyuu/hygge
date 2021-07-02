aaaf4d538d1107904121d8e66dd8f3e927390d61
tiiyuu <tiiyuu@users.noreply.github.com>
2021-07-02 07:34:43 +0000

    deploy: 67ee4d060f8d323e2e3fca3916a6bfa85cfb2bda

--- js/service_section_preview.js
+++ js/service_section_preview.js
@@ -57,7 +57,7 @@ const ServiceSectionPreview = ({ entry, getAsset, widgetFor, widgetsFor }) => {
                                         </div>
                                     </div>
                                     <div class="mt-5">
-                                        { widgetFor('content') }
+                                        { 'content' }
                                     </div>
                                 </div>
                                 )
