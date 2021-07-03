2388e7250f3253a7883e46b0734b9d57e290bb82
tiiyuu <gces5816@gmail.com>
2021-07-03 15:34:07 +0800

    add { tab1 ? }

--- static/js/resume_section_preview.js
+++ static/js/resume_section_preview.js
@@ -15,103 +15,52 @@ const ResumeSectionPreview = ({ entry, getAsset, widgetFor }) => {
         tab1 = false;
     }
 
-    if (tab1) {
-        return (
-            <>
-            <section class="section resume" id="resume">
-                <div class="resume__background"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="resume__heading">
-                                <div class="text-light">
-                                    <span class="top-title pre-line">{ data.topTitle }</span>
-                                    { widgetFor('title') }
-                                </div>
-
-                                <div class="btn-group nav mt-5">
-                                    <a id="tab1" class="btn btn-primary active" data-toggle="tab" onClick={ tab1Click }>{ data.tab1Name }</a>
-                                    <a id="tab2" class="btn btn-primary" data-toggle="tab" onClick={ tab2Click }>{ data.tab2Name }</a>
-                                </div>
+    return (
+        <>
+        <section class="section resume" id="resume">
+            <div class="resume__background"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="resume__heading">
+                            <div class="text-light">
+                                <span class="top-title pre-line">{ data.topTitle }</span>
+                                { widgetFor('title') }
                             </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="tab-content ">
-                                <div id="tab1_content" class="resume__education tab-pane active" id={`${data.tab1Target}`}>
-                                    { data.education.map((eda) => (
-                                        <div class="resume__education_item">
-                                            <span class="pre-line text-primary"> { eda.time } </span>
-                                            { eda.content }
-                                        </div>
-                                    )) }
-                                </div>
 
-                                <div id="tab2_content" class="resume__education tab-pane" id={`${data.tab2Target}`}>
-                                    { data.experience.map(exp => (
-                                        <div class="resume__education_item">
-                                            <span class="pre-line text-primary"> { exp.time } </span>
-                                            { exp.content }
-                                        </div>
-                                    )) }
-                                </div>
-
+                            <div class="btn-group nav mt-5">
+                                <a id="tab1" class="btn btn-primary active" data-toggle="tab" onClick={ tab1Click }>{ data.tab1Name }</a>
+                                <a id="tab2" class="btn btn-primary" data-toggle="tab" onClick={ tab2Click }>{ data.tab2Name }</a>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            </>
-        )
-    }
-    else {
-        return (
-            <>
-            <section class="section resume" id="resume">
-                <div class="resume__background"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="resume__heading">
-                                <div class="text-light">
-                                    <span class="top-title pre-line">{ data.topTitle }</span>
-                                    { widgetFor('title') }
-                                </div>
-
-                                <div class="btn-group nav mt-5">
-                                    <a id="tab1" class="btn btn-primary active" data-toggle="tab" onClick={ tab1Click }>{ data.tab1Name }</a>
-                                    <a id="tab2" class="btn btn-primary" data-toggle="tab" onClick={ tab2Click }>{ data.tab2Name }</a>
-                                </div>
+                    <div class="col-lg-6">
+                        <div class="tab-content ">
+                            <div id="tab1_content" class={ tab1 ? "resume__education tab-pane active" : "resume__education tab-pane"} id={`${data.tab1Target}`}>
+                                { data.education.map((eda) => (
+                                    <div class="resume__education_item">
+                                        <span class="pre-line text-primary"> { eda.time } </span>
+                                        { eda.content }
+                                    </div>
+                                )) }
                             </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="tab-content ">
-                                <div id="tab1_content" class="resume__education tab-pane" id={`${data.tab1Target}`}>
-                                    { data.education.map((eda) => (
-                                        <div class="resume__education_item">
-                                            <span class="pre-line text-primary"> { eda.time } </span>
-                                            { eda.content }
-                                        </div>
-                                    )) }
-                                </div>
 
-                                <div id="tab2_content" class="resume__education tab-pane active" id={`${data.tab2Target}`}>
-                                    { data.experience.map(exp => (
-                                        <div class="resume__education_item">
-                                            <span class="pre-line text-primary"> { exp.time } </span>
-                                            { exp.content }
-                                        </div>
-                                    )) }
-                                </div>
-
+                            <div id="tab2_content" class={ tab1 ? "resume__education tab-pane" : "resume__education tab-pane active"} id={`${data.tab2Target}`}>
+                                { data.experience.map(exp => (
+                                    <div class="resume__education_item">
+                                        <span class="pre-line text-primary"> { exp.time } </span>
+                                        { exp.content }
+                                    </div>
+                                )) }
                             </div>
+
                         </div>
                     </div>
                 </div>
-            </section>
-            </>
-        )
-    }
-    
+            </div>
+        </section>
+        </>
+    )
 }
 
 CMS.registerPreviewTemplate('resume', ResumeSectionPreview)
